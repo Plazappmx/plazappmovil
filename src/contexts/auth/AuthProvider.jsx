32318b3a1b05ba5d7fe96e6a33ce7ecd38838122
chrod <chrod.dev@gmail.com>
2025-03-23 18:27:38 -0500

    Add auth context

---
+++ src/contexts/auth/AuthProvider.jsx
@@ -0,0 +1,33 @@
+import { useEffect, useState } from "react";
+import AuthContext from "./AuthContext";
+import { onAuthStateChanged } from "firebase/auth";
+import { auth, db } from "../../config/firebase";
+import { doc, getDoc } from "firebase/firestore";
+
+const AuthProvider = ({ children }) => {
+  const [loggedUser, setLoggedUser] = useState(null);
+  const [isAuth, setIsAuth] = useState(false);
+
+  useEffect(() => {
+    onAuthStateChanged(auth, async (currentUser) => {
+      try {
+        if (currentUser) {
+          const userDoc = await getDoc(doc(db, "users", currentUser.uid));
+          setLoggedUser(userDoc.data());
+          setIsAuth(true);
+        } else {
+          setLoggedUser(null);
+          setIsAuth(false);
+        }
+      } catch (error) {
+        console.error(error.message);
+      }
+    });
+  }, []);
+
+  const data = { isAuth, loggedUser, setLoggedUser };
+
+  return <AuthContext.Provider value={data}>{children}</AuthContext.Provider>;
+};
+
+export default AuthProvider;
