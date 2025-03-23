6a35ad41839753d85fff86c8cc95826e8b4b8780
chrod <chrod.dev@gmail.com>
2025-03-23 16:22:16 -0500

    Update stack

--- App.js
+++ App.js
@@ -1,13 +1,22 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
 import RootStack from "./src/navigators/RootStack";
-
+import { SafeAreaView } from "react-native-safe-area-context";
+import { StyleSheet } from "react-native";
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <StatusBar style="auto" />
-      <RootStack />
-    </NavigationContainer>
-  )
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <StatusBar style="auto" />
+        <RootStack />
+      </NavigationContainer>
+    </SafeAreaView>
+  );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
