2defa377a69af0d13cce1aacaab46aa57e10ed13
chrod <chrod.dev@gmail.com>
2025-03-23 16:22:35 -0500

    Update stack 2

--- App.js
+++ App.js
@@ -1,15 +1,34 @@
 import { StatusBar } from "expo-status-bar";
 import { NavigationContainer } from "@react-navigation/native";
-import RootStack from "./src/navigators/RootStack";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { StyleSheet } from "react-native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
+import LoginScreen from "./src/screens/LoginScreen";
+import ForgotPasswordScreen from "./src/screens/ForgotPasswordScreen";
+import DashboardScreen from "./src/screens/DashboardScreen";
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <SafeAreaView style={styles.container}>
       <NavigationContainer>
         <StatusBar style="auto" />
-        <RootStack />
+        <Stack.Navigator
+          initialRouteName="Login"
+          screenOptions={{ headerShown: false }}
+        >
+          <Stack.Screen name="Login" component={LoginScreen} />
+          <Stack.Screen
+            name="ForgotPassword"
+            component={ForgotPasswordScreen}
+          />
+          <Stack.Screen
+            name="Dashboard"
+            component={DashboardScreen}
+            option={{ tabBarVisible: false }}
+          />
+        </Stack.Navigator>
       </NavigationContainer>
     </SafeAreaView>
   );
